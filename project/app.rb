df2f5a418fb4c2cce60fbdc829ad3c071218c6ec
Liam <95085039+liam-leikeryd-nti-johanneberg@users.noreply.github.com>
2023-02-03 11:18:14 +0100

    forms
    keept expanding the forms

--- project/app.rb
+++ project/app.rb
@@ -3,11 +3,37 @@ require 'slim'
 require 'sqlite3'
 require 'bcrypt'
 require 'sinatra/reloader'
+require_relative './model.rb'
 
 get('/') do
     slim('layout')
 end
 
-get('/setup/new') do
+get('/setup_new') do
     slim(:'setup_new')
+end
+
+post('/setup/new') do
+    woods_brand = params[:woods_brand]
+
+    if (woods_brand == "Titleist")
+        redirect('/setup_new/titleist')
+    
+    elsif (woods_brand == "Callaway")
+            redirect('/setup_new/callaway')
+
+    elsif (woods_brand == "Taylormade")
+            redirect('/setup_new/taylormade')
+
+    elsif (woods_brand == Cobra)
+            redirect('/setup_new/cobra')
+    end
+   
+   
+    
+end
+
+get('setup_new/titleist') do
+    slim(:'setup_new/titleist')
+    
 end
